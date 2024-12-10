f7d6445445b472e0a2ad084b9f7801773a6078cd
Emerson Ferreira <emecatarino@yahoo.com.br>
2024-12-09 21:36:45 -0300

    Componente Blade para exibir os Tweets

---
+++ resources/views/components/tweets.blade.php
@@ -0,0 +1,32 @@
+<div class="p-6 lg:p-8 border-b border-gray-200 bg-gray-200 bg-opacity-25">
+
+    <h1 class="mt-8 text-2xl font-medium text-gray-900">
+        Veja seus Tweets!
+    </h1>
+
+    <p class="mt-6 text-gray-500 leading-relaxed">
+        Usuário: Pensamento:
+    </p>
+</div>
+
+<div class="p-6 lg:p-8 border-b border-gray-200 bg-gray-200 bg-opacity-25">
+    <ul role="list" class="divide-y divide-gray-100">
+        <li class="flex justify-between gap-x-6 py-5">
+            <div class="flex min-w-0 gap-x-4"> <!-- foto -->
+                <img class="ml-4 size-12 flex-none rounded-full bg-gray-50"
+                     src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                <div class="min-w-0 flex-auto ml-4">
+                    <p class="text-sm/6 font-semibold text-gray-900">Amanda Santos</p> <!-- nome -->
+                    <p class="mt-1  text-xs/5 text-gray-500">amanda.santos@example.com</p> <!-- e-mail -->
+                </div>
+            </div>
+            <div class="mr-2 ">
+                <p class="ext-sm/6 text-gray-800">
+                    Tweet... No master (local atual) foi gerada uma Hech código que identifica a versão caso seja necessário recuperá-la ou voltar para ela – identificação da versão
+                    Foi adicionado um arquivo, tendo um arquivo modificado com 3 inserções, no modo de criação.
+                </p> <!-- tweet -->
+                <p class="text-gray-600 font-semibold text-right mt-1 mr-2">Curtir</p> <!-- curtida -->
+            </div>
+        </li>
+    </ul>
+</div>
